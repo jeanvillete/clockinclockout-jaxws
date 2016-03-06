ee1b229dcfddbc196cd7be7fe8e5fac48d899d60
Jean Villete <brunopib@gmail.com>
2016-03-05 21:24:55 -0300

    #12 providing an util method to allow set from Duration java datatype to database interval

--- src/main/java/org/com/clockinclockout/rowmapper/RowMapperUtil.java
+++ src/main/java/org/com/clockinclockout/rowmapper/RowMapperUtil.java
@@ -2,6 +2,9 @@ package org.com.clockinclockout.rowmapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.Duration;
+
+import org.postgresql.util.PGInterval;
 
 abstract class RowMapperUtil {
 
@@ -20,6 +23,11 @@ abstract class RowMapperUtil {
 		if ( rs.wasNull() ) return null;
 		strValue = strValue.trim().toLowerCase();
 		return strValue.equals( "true" ) || strValue.equals( "y" ) || strValue.equals( "1" );
+	}
+	
+	public static Duration getDuration( ResultSet rs, String column ) throws SQLException {
+		PGInterval pgInterval = ( PGInterval ) rs.getObject( column );
+		return pgInterval != null ? Duration.ofSeconds( ( pgInterval.getHours() * 3600 ) + ( pgInterval.getMinutes() * 60 ) + (int) pgInterval.getSeconds() ) : null;
 	}
 	
 }
