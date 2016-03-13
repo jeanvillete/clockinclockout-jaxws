1210aa95ba52211b0356eae50d78322963f94a1d
Jean Villete <brunopib@gmail.com>
2016-03-13 12:11:56 -0300

    fixes #19, concluding the service UserWSImpl as requested.

---
+++ src/main/java/com/clkio/ws/ObjectFactory.java
@@ -0,0 +1,68 @@
+
+package com.clkio.ws;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+import com.clkio.ws.domain.common.Response;
+import com.clkio.ws.domain.common.ResponseException;
+import com.clkio.ws.domain.user.User;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the com.clkio.ws package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _Response_QNAME = new QName("http://ws.clkio.com", "response");
+    private final static QName _ResponseException_QNAME = new QName("http://ws.clkio.com", "responseException");
+    private final static QName _User_QNAME = new QName("http://ws.clkio.com", "user");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.clkio.ws
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Response }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws.clkio.com", name = "response")
+    public JAXBElement<Response> createResponse(Response value) {
+        return new JAXBElement<Response>(_Response_QNAME, Response.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ResponseException }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws.clkio.com", name = "responseException")
+    public JAXBElement<ResponseException> createResponseException(ResponseException value) {
+        return new JAXBElement<ResponseException>(_ResponseException_QNAME, ResponseException.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link User }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws.clkio.com", name = "user")
+    public JAXBElement<User> createUser(User value) {
+        return new JAXBElement<User>(_User_QNAME, User.class, null, value);
+    }
+
+}
--- src/main/java/com/clkio/ws/impl/UserWSImpl.java
+++ src/main/java/com/clkio/ws/impl/UserWSImpl.java
@@ -1,10 +1,34 @@
 package com.clkio.ws.impl;
 
+import java.util.Locale;
+
 import javax.jws.WebService;
 
-import com.clkio.ws.UserWS;
+import org.apache.log4j.Logger;
 
-@WebService( endpointInterface = "com.clkio.ws.UserWS" )
-public class UserWSImpl extends WebServiceCommon implements UserWS {
+import com.clkio.domain.Email;
+import com.clkio.service.UserService;
+import com.clkio.ws.ResponseException;
+import com.clkio.ws.UserPort;
+import com.clkio.ws.domain.common.Response;
+import com.clkio.ws.domain.user.User;
 
+@WebService( endpointInterface = "com.clkio.ws.UserPort" )
+public class UserWSImpl extends WebServiceCommon implements UserPort {
+
+	private static final Logger LOG = Logger.getLogger( UserWSImpl.class );
+	
+	@Override
+	public Response insert( User userParam ) throws ResponseException {
+		try {
+			com.clkio.domain.User newUser = new com.clkio.domain.User( new Email( userParam.getEmail() ), new Locale( userParam.getLocale() ) );
+			newUser.setPassword( userParam.getPassword() );
+			this.getService( UserService.class ).insert( newUser );
+			
+			return new Response( "User persisted successfully. An email is going to be sent to you in order to confirm it, please, check your inbox." );
+		} catch ( Exception e ) {
+			LOG.error( e );
+			throw new ResponseException( e.getMessage(), new com.clkio.ws.domain.common.ResponseException() );
+		}
+	}
 }
