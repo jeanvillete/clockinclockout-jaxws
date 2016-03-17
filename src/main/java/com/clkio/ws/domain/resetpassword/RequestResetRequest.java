efb31d17fc37db38fe8ade9b7ddec351a36b5c84
Jean Villete <brunopib@gmail.com>
2016-03-17 20:50:35 -0300

    #51 implementing the base for the ResetPassword WS Services

---
+++ src/main/java/com/clkio/ws/domain/resetpassword/RequestResetRequest.java
@@ -0,0 +1,63 @@
+
+package com.clkio.ws.domain.resetpassword;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+import com.clkio.ws.domain.email.Email;
+
+
+/**
+ * <p>Java class for requestResetRequest complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="requestResetRequest">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="email" type="{http://schemas.clkio.com/email}email"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "requestResetRequest", propOrder = {
+    "email"
+})
+public class RequestResetRequest {
+
+    @XmlElement(required = true)
+    protected Email email;
+
+    /**
+     * Gets the value of the email property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Email }
+     *     
+     */
+    public Email getEmail() {
+        return email;
+    }
+
+    /**
+     * Sets the value of the email property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Email }
+     *     
+     */
+    public void setEmail(Email value) {
+        this.email = value;
+    }
+
+}
