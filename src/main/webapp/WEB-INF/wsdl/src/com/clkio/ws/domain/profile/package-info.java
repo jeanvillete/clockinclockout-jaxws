0bb3b117bd544eb72e708156389279697fc7acbc
Jean Villete <brunopib@gmail.com>
2016-03-20 16:11:06 -0300

    fixes #30 use case implementd as requested.

--- src/main/webapp/WEB-INF/wsdl/src/com/clkio/ws/domain/profile/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.clkio.com/profile", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.clkio.ws.domain.profile;
--- src/main/webapp/WEB-INF/wsdl/src/com/clkio/ws/domain/user/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.clkio.com/user", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.clkio.ws.domain.user;
