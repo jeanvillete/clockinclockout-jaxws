c8807fd746ecf96e8b1532155437302cf9398b23
Jean Villete <brunopib@gmail.com>
2016-03-27 22:14:34 -0300

    #12 improving LocalDateTimeUtil.java facility class, dealing with LocalDate and LocalDateTime representations.

--- src/main/java/com/clkio/common/LocalDateTimeUtil.java
+++ src/main/java/com/clkio/common/LocalDateTimeUtil.java
@@ -1,13 +1,18 @@
 package com.clkio.common;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import java.time.ZoneId;
 
 abstract public class LocalDateTimeUtil {
 
 	public static final Timestamp getTimestamp( LocalDateTime localDateTime ) {
-		return localDateTime != null ? new Timestamp( localDateTime.toInstant( ZoneOffset.UTC ).getEpochSecond() * 1000 ) : null;
+		return localDateTime != null ? new Timestamp( localDateTime.atZone( ZoneId.systemDefault() ).toInstant().getEpochSecond() * 1000 ) : null;
+	}
+	
+	public static final Timestamp getTimestamp( LocalDate localDate ) {
+		return localDate != null ? getTimestamp( localDate.atStartOfDay() ) : null;
 	}
 	
 }
