adf61f62c1a11f0d6cc636fcead919a27ccbd8b4
Jean Villete <brunopib@gmail.com>
2016-04-01 22:15:15 -0300

    implementing base for backend TimeCardWsImpl artifact, responsable to receive the requests; #42 #43 #47 #48 #44 #49 #50 #45 #46 #41 #40

---
+++ src/main/java/com/clkio/ws/impl/TimeCardWSImpl.java
@@ -0,0 +1,200 @@
+package com.clkio.ws.impl;
+
+import javax.jws.WebService;
+
+import org.apache.log4j.Logger;
+import org.springframework.util.Assert;
+
+import com.clkio.ws.ResponseException;
+import com.clkio.ws.TimeCardPort;
+import com.clkio.ws.domain.common.Response;
+import com.clkio.ws.domain.timecard.DeleteClockinClockoutRequest;
+import com.clkio.ws.domain.timecard.DeleteManualEnteringRequest;
+import com.clkio.ws.domain.timecard.GetTimeCardRequest;
+import com.clkio.ws.domain.timecard.GetTimeCardResponse;
+import com.clkio.ws.domain.timecard.GetTotalTimeMonthlyRequest;
+import com.clkio.ws.domain.timecard.GetTotalTimeMonthlyResponse;
+import com.clkio.ws.domain.timecard.GetTotalTimeRequest;
+import com.clkio.ws.domain.timecard.GetTotalTimeResponse;
+import com.clkio.ws.domain.timecard.InsertClockinClockoutRequest;
+import com.clkio.ws.domain.timecard.InsertManualEnteringRequest;
+import com.clkio.ws.domain.timecard.PunchClockRequest;
+import com.clkio.ws.domain.timecard.SetExpectedHoursRequest;
+import com.clkio.ws.domain.timecard.SetNotesRequest;
+import com.clkio.ws.domain.timecard.UpdateClockinClockoutRequest;
+import com.clkio.ws.domain.timecard.UpdateManualEnteringRequest;
+
+@WebService( endpointInterface = "com.clkio.ws.TimeCardPort" )
+public class TimeCardWSImpl extends WebServiceCommon implements TimeCardPort {
+
+	private static final Logger LOG = Logger.getLogger( TimeCardWSImpl.class );
+	
+	@Override
+	public GetTotalTimeResponse getTotalTime( GetTotalTimeRequest request ) throws ResponseException {
+		try {
+			Assert.notNull( request );
+			Assert.state( request.getProfile() != null && request.getProfile().getId() != null,
+					"[clkiows] No 'profile' instance was found on the request or its 'id' property was not provided." );
+			
+			return null;
+		} catch ( Exception e ) {
+			LOG.error( e );
+			throw new ResponseException( e.getMessage(), new com.clkio.ws.domain.common.ResponseException() );
+		}
+	}
+
+	@Override
+	public GetTimeCardResponse getTimeCard( GetTimeCardRequest request ) throws ResponseException {
+		try {
+			Assert.notNull( request );
+			Assert.state( request.getProfile() != null && request.getProfile().getId() != null,
+					"[clkiows] No 'profile' instance was found on the request or its 'id' property was not provided." );
+			
+			return null;
+		} catch ( Exception e ) {
+			LOG.error( e );
+			throw new ResponseException( e.getMessage(), new com.clkio.ws.domain.common.ResponseException() );
+		}
+	}
+
+	@Override
+	public Response punchClock( PunchClockRequest request ) throws ResponseException {
+		try {
+			Assert.notNull( request );
+			Assert.state( request.getProfile() != null && request.getProfile().getId() != null,
+					"[clkiows] No 'profile' instance was found on the request or its 'id' property was not provided." );
+			
+			return null;
+		} catch ( Exception e ) {
+			LOG.error( e );
+			throw new ResponseException( e.getMessage(), new com.clkio.ws.domain.common.ResponseException() );
+		}
+	}
+
+	@Override
+	public Response insertClockinClockout( InsertClockinClockoutRequest request ) throws ResponseException {
+		try {
+			Assert.notNull( request );
+			Assert.state( request.getProfile() != null && request.getProfile().getId() != null,
+					"[clkiows] No 'profile' instance was found on the request or its 'id' property was not provided." );
+			
+			return null;
+		} catch ( Exception e ) {
+			LOG.error( e );
+			throw new ResponseException( e.getMessage(), new com.clkio.ws.domain.common.ResponseException() );
+		}
+	}
+
+	@Override
+	public Response updateClockinClockout( UpdateClockinClockoutRequest request ) throws ResponseException {
+		try {
+			Assert.notNull( request );
+			Assert.state( request.getProfile() != null && request.getProfile().getId() != null,
+					"[clkiows] No 'profile' instance was found on the request or its 'id' property was not provided." );
+			
+			return null;
+		} catch ( Exception e ) {
+			LOG.error( e );
+			throw new ResponseException( e.getMessage(), new com.clkio.ws.domain.common.ResponseException() );
+		}
+	}
+
+	@Override
+	public Response deleteClockinClockout( DeleteClockinClockoutRequest request ) throws ResponseException {
+		try {
+			Assert.notNull( request );
+			Assert.state( request.getProfile() != null && request.getProfile().getId() != null,
+					"[clkiows] No 'profile' instance was found on the request or its 'id' property was not provided." );
+			
+			return null;
+		} catch ( Exception e ) {
+			LOG.error( e );
+			throw new ResponseException( e.getMessage(), new com.clkio.ws.domain.common.ResponseException() );
+		}
+	}
+
+	@Override
+	public Response insertManualEntering( InsertManualEnteringRequest request ) throws ResponseException {
+		try {
+			Assert.notNull( request );
+			Assert.state( request.getProfile() != null && request.getProfile().getId() != null,
+					"[clkiows] No 'profile' instance was found on the request or its 'id' property was not provided." );
+			
+			return null;
+		} catch ( Exception e ) {
+			LOG.error( e );
+			throw new ResponseException( e.getMessage(), new com.clkio.ws.domain.common.ResponseException() );
+		}
+	}
+
+	@Override
+	public Response updateManualEntering( UpdateManualEnteringRequest request ) throws ResponseException {
+		try {
+			Assert.notNull( request );
+			Assert.state( request.getProfile() != null && request.getProfile().getId() != null,
+					"[clkiows] No 'profile' instance was found on the request or its 'id' property was not provided." );
+			
+			return null;
+		} catch ( Exception e ) {
+			LOG.error( e );
+			throw new ResponseException( e.getMessage(), new com.clkio.ws.domain.common.ResponseException() );
+		}
+	}
+
+	@Override
+	public Response deleteManualEntering( DeleteManualEnteringRequest request ) throws ResponseException {
+		try {
+			Assert.notNull( request );
+			Assert.state( request.getProfile() != null && request.getProfile().getId() != null,
+					"[clkiows] No 'profile' instance was found on the request or its 'id' property was not provided." );
+			
+			return null;
+		} catch ( Exception e ) {
+			LOG.error( e );
+			throw new ResponseException( e.getMessage(), new com.clkio.ws.domain.common.ResponseException() );
+		}
+	}
+
+	@Override
+	public Response setNotes( SetNotesRequest request ) throws ResponseException {
+		try {
+			Assert.notNull( request );
+			Assert.state( request.getProfile() != null && request.getProfile().getId() != null,
+					"[clkiows] No 'profile' instance was found on the request or its 'id' property was not provided." );
+			
+			return null;
+		} catch ( Exception e ) {
+			LOG.error( e );
+			throw new ResponseException( e.getMessage(), new com.clkio.ws.domain.common.ResponseException() );
+		}
+	}
+
+	@Override
+	public Response setExpectedHours( SetExpectedHoursRequest request ) throws ResponseException {
+		try {
+			Assert.notNull( request );
+			Assert.state( request.getProfile() != null && request.getProfile().getId() != null,
+					"[clkiows] No 'profile' instance was found on the request or its 'id' property was not provided." );
+			
+			return null;
+		} catch ( Exception e ) {
+			LOG.error( e );
+			throw new ResponseException( e.getMessage(), new com.clkio.ws.domain.common.ResponseException() );
+		}
+	}
+
+	@Override
+	public GetTotalTimeMonthlyResponse getTotalTimeMonthly( GetTotalTimeMonthlyRequest request ) throws ResponseException {
+		try {
+			Assert.notNull( request );
+			Assert.state( request.getProfile() != null && request.getProfile().getId() != null,
+					"[clkiows] No 'profile' instance was found on the request or its 'id' property was not provided." );
+			
+			return null;
+		} catch ( Exception e ) {
+			LOG.error( e );
+			throw new ResponseException( e.getMessage(), new com.clkio.ws.domain.common.ResponseException() );
+		}
+	}
+
+}
